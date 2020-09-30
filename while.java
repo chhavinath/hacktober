9a08a2c90522a643554d06ad0246561f81d02243
chhavinath <72135110+chhavinath@users.noreply.github.com>
2020-09-30 17:05:57 +0530

    Create while.java

---
+++ while.java
@@ -0,0 +1,10 @@
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int cases = sc.nextInt();
+        int m, n;
+
+        while (cases-- > 0)
+            System.out.println("this is while loop");
+    }
+}
